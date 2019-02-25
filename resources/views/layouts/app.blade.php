e97ba0f57a1b0ae6b72fdf19fe06abfb32fc50a5
Vukasin <vukasin.babic@eng.rs>
2019-02-25 13:01:00 +0100

    initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Basic App</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+    @include('inc.navbar')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-lg-8">
+                    @yield('content')
+            </div>
+            <div class="col-md-8 col-lg-4">
+                    @include('inc.sidebar')
+            </div>
+        </div>
+    </div>
+</body>
+</html>
